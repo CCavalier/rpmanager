e1ff61ea9e12e2f222c6b3fec7b2c4c1459b608c
Cavalier Pont <charlotte.cavalier@gmail.com>
2024-08-16 07:51:03 +0200

    first commit

---
+++ src/main/java/com/potatoes/rpmanager/RpmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.potatoes.rpmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/potatoes/rpmanager/RpmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potatoes.rpmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
